7ba6ecfd05ddc3fcbf40b242739497bf102b9fa7
10151260 <yeon_jae.kim@kt.com>
2022-09-12 11:50:46 +0900

    [ADD] Body Decorator, Query Parameter

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param,Patch,Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param,Patch,Post, Put, Query } from '@nestjs/common';
 import { create } from 'domain';
 
 @Controller('movies')
@@ -8,24 +8,35 @@ export class MoviesController {
         return 'this will return all movies';
     }
 
-    @Get('/:id')
+    @Get('search')
+    search(@Query('year') searchYear:string) {
+        return `We are searching for a movie made after ${searchYear}`
+    }
+
+    @Get(':id')
     getOne(@Param('id') movieId: string) { // @Param에 들어있는 인자와 위의 get :id는 같아야 한다. 
         //id라는 parameter를 movieId라는 argument에 string 타입으로저장하고 싶어
         return `This will return one movie with the id : ${movieId}`;
     }
-
+   
     @Post()
-    create(){
+    create(@Body() movieData){
+        console.log(movieData)
         return 'this will create a movie';
     }
 
-    @Delete('/:id')
+    @Delete(':id')
     remove(@Param('id') movieId: string) {
         return `this will remove a movie with the id : ${movieId}`;
     }
 
     @Patch(':id')
-    update(@Param('id') movieId: string) {
-        return `this will update a movie with the id : ${movieId}`;
+    update(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updateMovie: movieId,
+            ... updateData
+        }
     }
+    
+    
 }
